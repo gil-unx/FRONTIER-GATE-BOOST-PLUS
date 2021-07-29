264c6fe71d89a0153c938a397b5964d3a373b907
gil-unx <57790408+gil-unx@users.noreply.github.com>
2021-07-28 23:24:11 -0700

    Add files via upload

---
+++ repack_bin.bat
@@ -0,0 +1,5 @@
+crifilesystem\cpkmakec.exe CSV\000.csv USRDIR_NEW\000.bin 
+crifilesystem\cpkmakec.exe CSV\008.csv USRDIR_NEW\008.bin 
+crifilesystem\cpkmakec.exe CSV\009.csv USRDIR_NEW\009.bin 
+crifilesystem\cpkmakec.exe CSV\011.csv USRDIR_NEW\011.bin 
+crifilesystem\cpkmakec.exe CSV\014.csv USRDIR_NEW\014.bin 
---
+++ unpack_bin.bat
@@ -0,0 +1,5 @@
+crifilesystem\cpkmakec.exe USRDIR\000.bin -extract=USRDIR\000
+crifilesystem\cpkmakec.exe USRDIR\008.bin -extract=USRDIR\008
+crifilesystem\cpkmakec.exe USRDIR\009.bin -extract=USRDIR\009
+crifilesystem\cpkmakec.exe USRDIR\011.bin -extract=USRDIR\011
+crifilesystem\cpkmakec.exe USRDIR\014.bin -extract=USRDIR\014
